b07d85111ee7d871262b0417d62272e5c2accb5d
NishantChauhan032 <nishantkr0210@gmail.com>
2020-09-26 19:08:40 +0530

    Initial Position Shown

--- game.java
+++ game.java
@@ -1,5 +1,6 @@
 public class game{
   public static void main(String[] args){
-    System.out.println("Welcome to Snake And Ladder Game!");
+    int position=0;
+    System.out.println("Welcome to Snake And Ladder Game!\n Your initial position is :"+position);
     }
-}
+ }
