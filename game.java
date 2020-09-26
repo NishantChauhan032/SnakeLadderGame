e28533707721c495085dd072d4a093ed3b57318e
NishantChauhan032 <nishantkr0210@gmail.com>
2020-09-26 19:18:02 +0530

    Dice Rolled

--- game.java
+++ game.java
@@ -2,5 +2,8 @@ public class game{
   public static void main(String[] args){
     int position=0;
     System.out.println("Welcome to Snake And Ladder Game!\n Your initial position is :"+position);
+    int number=(int)Math.floor(Math.random() * 10) % 6;
+    System.out.println("You Got :" +(number+1));
+
     }
  }
