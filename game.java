d5ca64058332f7d2545aaecec0763f5d24fcf33a
NishantChauhan032 <nishantkr0210@gmail.com>
2020-09-26 19:27:29 +0530

    Option Checked

--- game.java
+++ game.java
@@ -4,6 +4,15 @@ public class game{
     System.out.println("Welcome to Snake And Ladder Game!\n Your initial position is :"+position);
     int number=(int)Math.floor(Math.random() * 10) % 6;
     System.out.println("You Got :" +(number+1));
-
+    
+    int chance= (int)Math.floor(Math.random() * 10) % 3;
+      switch(chance){
+       case 0: System.out.println("No Play");
+       break;
+       case 1: System.out.println("Its a Ladder, Move Forward");
+       break;
+       case 2: System.out.println("Its a Snake, Move Backward");
+       break;
+       }
     }
  }
